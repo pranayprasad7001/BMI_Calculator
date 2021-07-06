866722a3c75436be50afca15fa6119d0174a0e20
pranayprasad7001 <68220629+pranayprasad7001@users.noreply.github.com>
2021-07-06 07:10:56 +0530

    added if-elif-else, and made it better

--- Bmi_Calculator.py
+++ Bmi_Calculator.py
@@ -2,6 +2,16 @@ print("BMI Calculator:\n")
 # Taking your height and weight as input and type casting it into float
 height = float(input("Enter your height in m: "))
 weight = float(input("Enter your weight in kg: "))
-# Calculating their BMI & limiting it decimal digit upto 1.
-bmi = round((weight / (height ** 2)), 1)
-print("\nYour Body Mass Index (BMI) is: " + str(bmi))
+# Calculating their BMI & rounding off the result.
+bmi = round(weight / height ** 2)
+if bmi < 18.5:
+  print(f"Your BMI is {bmi}, you are underweight.")
+elif bmi < 25:
+  print(f"Your BMI is {bmi}, you have a normal weight.")
+elif bmi < 30:
+  print(f"Your BMI is {bmi}, you are slightly overweight.")
+elif bmi < 35:
+  print(f"Your BMI is {bmi}, you are obese.")
+else:
+  print(f"Your BMI is {bmi}, you are clinically obese.")
+
